64e43aa344e66ce4009bfe22d102a9a440e608ad
Doniyor <xoja9636706@gmail.com>
2020-08-13 18:04:31 +0500

    leave  commentda return render bo`masligi kk edi

--- myfirst/apps/articles/views.py
+++ myfirst/apps/articles/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from.models import Article
+from.models import Article,Comment
 
 def index(request):
     latest_articles_list = Article.objects.order_by('-pub_date')[:5]
@@ -12,14 +12,17 @@ def detail(request, article_id):
         a = Article.objects.get( id = article_id )
     except:
         raise Http4004("Sahifa topilmadi!")
-    return render(request, 'articles/detail.html', {'article':a})
-
     latest_comments_list = a.comment_set.order_by('-id')[:]
+    
+    context  = {'article':a,"latest_comments_list":latest_comments_list}
+
+    return render(request, 'articles/detail.html', context)
+
 def leave_comment(request, article_id):
     try:
         a = Article.objects.get(id=article_id)
     except:
         raise Http4004("Sahifa topilmadi!")
-    return render(request, 'articles/detail.html', {'article': a})
+    
     a.comment_set.create(author_name =request.POST['name'], comment_text = request.POST['text'])
     return HttpResponseRedirect( reverse('articles:detail', args = (a.id,)) )
